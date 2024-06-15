65ba9e3fe3d1cf9ecd4a8652c0405ee0d5163bcc
gabrielliosc <gabrielli.osc@gmail.com>
2024-06-15 16:36:11 -0300

    feat: sample 1 and sample 2

---
+++ src/Sample1/PrintClass.java
@@ -0,0 +1,7 @@
+package Sample1;
+
+public class PrintClass {
+    public static void main(String[] args){
+        System.out.println("Java drives the Web");
+    }
+}
---
+++ src/Sample1/Variables.java
@@ -0,0 +1,18 @@
+package Sample1;
+
+public class Variables {
+    public static void main(String[] args) {
+//Pode ser inicializado da seguinte fora por ser dois de mesmo tipo: int var1, var2;
+        int var1;
+        int var2;
+
+        var1 = 1024;
+
+        System.out.println("var1 contains " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.print("var2 contains var1 / 2: ");
+        System.out.println(var2);
+    }
+}
